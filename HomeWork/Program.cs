6fe015fd2243746158004eccc9a8ac61844b3c6d
ismayilesedov <ismayilras@code.edu.az>
2022-12-05 10:30:15 +0400

    init

---
+++ HomeWork/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 26;
+            int b = 39;
+            Console.WriteLine(a + b);
+        }
+    }
+}
