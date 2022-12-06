63392112a6073c25de41c008d75dcf5b02e11b90
ismayilras@code.edu.az <ismayilras@code.edu.az>
2022-12-06 11:32:33 +0400

    New data hw 2

--- HomeWork2/HomeWork2/Program.cs
+++ HomeWork2/HomeWork2/Program.cs
@@ -8,7 +8,7 @@
             int a=Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("b ededini daxil edin");
             int b=Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(a%b);
+            Console.WriteLine("Qaliq:"+ (a%b));
         }
     }
 }
